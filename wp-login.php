f270c9c2af9f3375812f92ba4b241b21a8deaf02
Pashtet365 <vlad.clohara.posedni@gmail.com>
2022-07-30 14:19:47 +0300

    Fix css

--- wp-login.php
+++ wp-login.php
@@ -5,11 +5,11 @@
 	<title>Войти &lsaquo; Golubev_Pavel &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="/https://pashtet365.github.io/WordPress/wp-includes/css/dashicons.min.css?ver=6.0.1" media="all">
-<link rel="stylesheet" id="buttons-css" href="/https://pashtet365.github.io/WordPress/wp-includes/css/buttons.min.css?ver=6.0.1" media="all">
-<link rel="stylesheet" id="forms-css" href="/https://pashtet365.github.io/WordPress/wp-admin/css/forms.min.css?ver=6.0.1" media="all">
-<link rel="stylesheet" id="l10n-css" href="/https://pashtet365.github.io/WordPress/wp-admin/css/l10n.min.css?ver=6.0.1" media="all">
-<link rel="stylesheet" id="login-css" href="/https://pashtet365.github.io/WordPress/wp-admin/css/login.min.css?ver=6.0.1" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://pashtet365.github.io/WordPress/wp-includes/css/dashicons.min.css?ver=6.0.1" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://pashtet365.github.io/WordPress/wp-includes/css/buttons.min.css?ver=6.0.1" media="all">
+<link rel="stylesheet" id="forms-css" href="https://pashtet365.github.io/WordPress/wp-admin/css/forms.min.css?ver=6.0.1" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://pashtet365.github.io/WordPress/wp-admin/css/l10n.min.css?ver=6.0.1" media="all">
+<link rel="stylesheet" id="login-css" href="https://pashtet365.github.io/WordPress/wp-admin/css/login.min.css?ver=6.0.1" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -20,7 +20,7 @@
 		<div id="login">
 		<h1><a href="https://ru.wordpress.org/">Сайт работает на WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://pashtet365.github.io/WordPress/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Имя пользователя или email</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
@@ -38,20 +38,20 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Запомнить меня</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Войти">
-									<input type="hidden" name="redirect_to" value="/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://pashtet365.github.io/WordPress/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="/https://pashtet365.github.io/WordPress/wp-login.php?action=lostpassword">Забыли пароль?</a>
+								<a href="https://pashtet365.github.io/WordPress/wp-login.php?action=lostpassword">Забыли пароль?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="/https://pashtet365.github.io/WordPress/">&larr; Перейти к Golubev_Pavel</a>		</p>
+			<a href="https://pashtet365.github.io/WordPress/">&larr; Перейти к Golubev_Pavel</a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
@@ -70,16 +70,16 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 
 					</form>
 				</div>
-				<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+				<script src="https://pashtet365.github.io/WordPress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script id="zxcvbn-async-js-extra">
-var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/pashtet365.github.io\/WordPress\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -93,12 +93,12 @@ var pwsL10n = {"unknown":"Надёжность пароля неизвестна
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-06-21 06:13:50+0000","generator":"GlotPress\/4.0.0-alpha.1","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=3; plural=(n % 10 == 1 && n % 100 != 11) ? 0 : ((n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 12 || n % 100 > 14)) ? 1 : 2);","lang":"ru"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s устарела с версии %2$s! Вместо неё используйте %3$s. Пожалуйста, рассмотрите возможность написания более инклюзивного кода."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script src="/https://pashtet365.github.io/WordPress/wp-admin/js/password-strength-meter.min.js?ver=6.0.1" id="password-strength-meter-js"></script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-admin/js/password-strength-meter.min.js?ver=6.0.1" id="password-strength-meter-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
 <script id="wp-util-js-extra">
 var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src="/https://pashtet365.github.io/WordPress/wp-includes/js/wp-util.min.js?ver=6.0.1" id="wp-util-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-includes/js/wp-util.min.js?ver=6.0.1" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
 var userProfileL10n = {"user_id":"0","nonce":"0bf0e72c60"};
 </script>
@@ -109,7 +109,7 @@ var userProfileL10n = {"user_id":"0","nonce":"0bf0e72c60"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-06-21 06:13:50+0000","generator":"GlotPress\/4.0.0-alpha.1","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=3; plural=(n % 10 == 1 && n % 100 != 11) ? 0 : ((n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 12 || n % 100 > 14)) ? 1 : 2);","lang":"ru"},"Your new password has not been saved.":["Ваш новый пароль не был сохранён."],"Hide":["Скрыть"],"Show":["Показать"],"Confirm use of weak password":["Разрешить использование слабого пароля."],"Hide password":["Скрыть пароль"],"Show password":["Показать пароль"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script src="/https://pashtet365.github.io/WordPress/wp-admin/js/user-profile.min.js?ver=6.0.1" id="user-profile-js"></script>
+<script src="https://pashtet365.github.io/WordPress/wp-admin/js/user-profile.min.js?ver=6.0.1" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
