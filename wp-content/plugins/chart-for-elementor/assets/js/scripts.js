72da5b9ac4f0d44248e0d600e6ebd76e7083b413
Pashtet365 <vlad.clohara.posedni@gmail.com>
2022-08-06 11:31:59 +0300

    Site Buisness Card
    all upload

---
+++ wp-content/plugins/chart-for-elementor/assets/js/scripts.js
@@ -0,0 +1,40 @@
+(function($){
+'use strict';
+
+// /*----- ELEMENTOR LOAD FUNTION CALL ---*/
+
+// $( window ).on( 'elementor/frontend/init', function() {
+
+
+//     var odometer = function(){
+     
+//   /*------------------------------------------
+//         = FUNFACT
+//     -------------------------------------------*/
+//     if ($(".odometer").length) {
+//         $('.odometer').appear();
+//         $(document.body).on('appear', '.odometer', function(e) {
+//             var odo = $(".odometer");
+//             odo.each(function() {
+//                 var countNumber = $(this).attr("data-count");
+//                 $(this).html(countNumber);
+//             });
+//         });
+//     }
+
+
+//     }; // end
+
+   
+
+//     //odometer
+//     elementorFrontend.hooks.addAction( 'frontend/element_ready/wpo-edefy_team.default', function($scope, $){
+//         odometer();
+//     } );
+
+
+    
+// } );
+
+
+})(jQuery);  
